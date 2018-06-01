c498a43c1b106c0e1b1e1a9b807601128a3b1cf5
Liz House <lizehouse@gmail.com>
2018-06-01 12:43:49 -0600

    picture edits and flex styling

--- client/src/components/StoryBoard.js
+++ client/src/components/StoryBoard.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Header, Container, Divider } from 'semantic-ui-react';
+import { Header, Container, Divider, Image } from 'semantic-ui-react';
 import { connect } from 'react-redux';
 import { getStories } from '../actions/stories';
 
@@ -23,6 +23,11 @@ class StoryBoard extends React.Component {
               <br />
               <Header as='h3'>{story.title}</Header>
               <Header as='h4'>Author: {story.author}</Header>
+              <div style={styles.images}>
+                <Image src={story.image_one} style={styles.imageSingle} />
+                <Image src={story.image_two} style={styles.imageSingle} />
+                <Image src={story.image_three} style={styles.imageSingle} />
+              </div>
               <p>{story.body}</p>
             </div>
             )}
@@ -36,7 +41,15 @@ const styles = {
   storyBox: {
     backgroundColor: 'yellow',
     padding: '50px',
-  }
+  },
+  images: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+  },
+  imageSingle: {
+    width: '220px',
+  },
 }
 
 const mapStateToProps = (state) => {
